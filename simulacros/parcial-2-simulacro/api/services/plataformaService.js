165b1bcbc1ec9f2b9bc900210f9074299dcb2602
Santiago Guillermo Stutz <401863@sistemas.frc.utn.edu.ar>
2025-10-27 19:03:21 -0300

    .

--- simulacros/parcial-2-simulacro/api/services/plataformaService.js
+++
@@ -1,9 +0,0 @@
-import plataformaRepository from "../repositories/plataformaRepository.js";
-
-class PlataformaService {
-  async obtenerTodas() {
-    return await plataformaRepository.obtenerTodos();
-  }
-}
-
-export default new PlataformaService();
